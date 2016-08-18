1c14534f67b1a7034aaf23b357e8c733789dc015
Aguirregzz97 <aguirregzz97@gmail.com>
2016-08-18 14:44:21 -0500

    Finish 1st homework

--- ConvertidorMetrosAPies/main.cpp
+++ ConvertidorMetrosAPies/main.cpp
@@ -1,19 +1,35 @@
+/* This program converts your given input in meters to an output in Feet
+Andres Aguirre Gonzalez A01039656 8/18/2016
+13 lines of code
+execution time: 1.1 seconds
+
+Actual test
+given: 2 meters
+expected: 6.5 feet
+
+*/
+
+
+
+
+
 #include <iostream>
 
 using namespace std;
 
 int main()
 {
+    //declare variables
     double dMetersInput;
     const double dMetersConversion = 100;
     const double dInchConversion = 2.54;
     const double dFeetConversion = 12;
     double dFeetOutput;
 
-
+    //start coding
     cout << "Please give me how many meters you want to convert" << endl;
     cin >> dMetersInput;
     dFeetOutput = ((dMetersInput * dMetersConversion) / dInchConversion) / dFeetConversion;
-    cout << dMetersInput << "Meters are equivalent to" <<endl << dFeetOutput;
+    cout << endl << dMetersInput << "\t" << "Meters are equivalent to" <<endl << dFeetOutput <<"\t" << "Feet";
     return 0;
 }
